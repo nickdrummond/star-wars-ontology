db3d782254c546cef81018a6cf76189c3c221687
nickdrummond <nickdrummond@yahoo.com>
2022-06-12 20:15:32 +0100

    Convor bookmarklet

---
+++ src/convor.js
@@ -0,0 +1,11 @@
+javascript: (() => {
+    var currentURL = window.location.href;
+
+    var fragment = currentURL.indexOf("#");
+    var page = encodeURIComponent((fragment > -1) ? currentURL.substr(0, fragment) : currentURL);
+    var propURL = encodeURIComponent("http://www.w3.org/2000/01/seeAlso");
+
+    var searchOntUrl = "http://star-wars-ontology.herokuapp.com/entities/annotation?property=seeAlso&search=" + page;
+
+    window.open(searchOntUrl, '_blank');
+})();

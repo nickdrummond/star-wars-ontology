2e0f80e645da8faac9772e4eeb60cdc868133d56
nickdrummond <nickdrummond@yahoo.com>
2022-06-19 22:39:08 +0100

    Reified stole property which has now been removed and replaced with Stealing event. Review of Heist which is now a subclass of Stealing
    Ontologies processed in 7672 ms by Pellet
    
    part way to #15

--- src/main/java/com/nickd/sw/ReifyProperty.java
+++ src/main/java/com/nickd/sw/ReifyProperty.java
@@ -21,8 +21,8 @@ public class ReifyProperty {
     public static void main(String[] args) throws OWLOntologyCreationException, OWLOntologyStorageException {
         Helper helper = new Helper("all.owl.ttl", new StarWarsOntologiesIRIMapper());
         ReifyProperty reifyProperty = new ReifyProperty(
-                helper.prop("freeingOf"),
-                helper.cls("Rescue"),
+                helper.prop("stole"),
+                helper.cls("Stealing"),
                 helper.prop("of"),
                 helper.prop("included"));
         List<OWLOntologyChange> changes = reifyProperty.run(helper.mngr.getOntologies(), helper.df);

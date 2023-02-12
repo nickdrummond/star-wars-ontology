4541ec90c02291f562de6cb16bb40465198853dd
nickdrummond <nickdrummond@yahoo.com>
2023-02-12 21:47:36 +0000

    Abox refactor code

---
+++ src/main/java/com/nickd/sw/AboxRefactor.java
@@ -0,0 +1,237 @@
+package com.nickd.sw;
+
+import com.github.jsonldjava.shaded.com.google.common.collect.Sets;
+import com.google.common.collect.Maps;
+import openllet.core.utils.QNameProvider;
+import org.semanticweb.owlapi.manchestersyntax.renderer.ManchesterOWLSyntaxObjectRenderer;
+import org.semanticweb.owlapi.model.*;
+import org.semanticweb.owlapi.model.parameters.Imports;
+import org.semanticweb.owlapi.util.ShortFormProvider;
+import org.semanticweb.owlapi.util.SimpleShortFormProvider;
+
+import java.io.StringWriter;
+import java.util.*;
+
+public class AboxRefactor {
+
+    private OWLObjectProperty includedProperty;
+    private OWLObjectProperty duringProperty;
+
+    public static void main(String[] args) throws OWLOntologyCreationException, OWLOntologyStorageException {
+        Helper helper = new Helper("all.owl.ttl", new StarWarsOntologiesIRIMapper());
+        AboxRefactor reifyProperty = new AboxRefactor(helper.prop("included"), helper.prop("during"));
+        List<OWLOntologyChange> changes = reifyProperty.run(helper.mngr.getOntologies(), helper.df);
+
+        helper.mngr.applyChanges(changes);
+
+        helper.saveChanged();
+    }
+
+    public AboxRefactor(final OWLObjectProperty includedProperty,
+                        final OWLObjectProperty duringProperty) {
+        this.includedProperty = includedProperty;
+        this.duringProperty = duringProperty;
+    }
+
+    public List<OWLOntologyChange> run(final Set<OWLOntology> onts, OWLDataFactory df) {
+
+        List<OWLOntologyChange> changes = new ArrayList<>();
+
+        OWLAxiomTransformer owlAxiomTransformer = new OWLAxiomTransformer(df, new NameProvider());
+
+        onts.forEach( ont -> ont .getReferencingAxioms(includedProperty, Imports.EXCLUDED).forEach(ax -> {
+            Set<OWLAxiom> axTran = ax.accept(owlAxiomTransformer);
+            if (!axTran.isEmpty()) {
+                changes.add(new RemoveAxiom(ont, ax));
+                axTran.forEach(a -> changes.add(new AddAxiom(ont, a)));
+            }
+            else {
+                System.err.println("No transform performed on: " + render(ax));
+            }
+
+        }));
+        return changes;
+    }
+
+    public String render(OWLObject o) {
+        final ShortFormProvider sfp = new SimpleShortFormProvider();
+        StringWriter w = new StringWriter();
+        o.accept(new ManchesterOWLSyntaxObjectRenderer(w, sfp));
+        return w.toString();
+    }
+
+    private class OWLAxiomTransformer implements OWLAxiomVisitorEx<Set<OWLAxiom>> {
+
+        private final OWLDataFactory df;
+        private final NameProvider nameProvider;
+
+        private OWLAxiomTransformer(OWLDataFactory df, final NameProvider nameProvider) {
+            this.df = df;
+            this.nameProvider = nameProvider;
+        }
+
+        @Override
+        public Set<OWLAxiom> visit(OWLClassAssertionAxiom axiom) {
+            OWLIndividual event = axiom.getIndividual();
+            OWLClassToIndividual clsTransformer = new OWLClassToIndividual(df, event, nameProvider);
+            axiom.getClassExpression().accept(clsTransformer);
+            return clsTransformer.getAxioms();
+        }
+
+        @Override
+        public <T> Set<OWLAxiom> doDefault(T object) {
+            return Collections.emptySet();
+        }
+    }
+
+    private class NameProvider {
+
+        private final String BASE = "https://nickdrummond.github.io/star-wars-ontology/ontologies#";
+
+        private final Map<OWLIndividual, Integer> eventChildCount = Maps.newHashMap();
+
+        public IRI getName(OWLIndividual parentEvent) {
+            int count = eventChildCount.getOrDefault(parentEvent, 0);
+            eventChildCount.put(parentEvent, count+1);
+            return IRI.create(BASE + "_" + render(parentEvent).toLowerCase() + count);
+        }
+    }
+
+    private class OWLClassToIndividual implements OWLClassExpressionVisitor {
+
+        private final OWLDataFactory df;
+        private final OWLIndividual parentEvent;
+        private final NameProvider nameProvider;
+
+        private final Set<OWLAxiom> axioms = Sets.newHashSet();
+
+        private OWLIndividual subEvent = null;
+
+        private OWLClassToIndividual(final OWLDataFactory df,
+                                     final OWLIndividual parentEvent,
+                                     final NameProvider nameProvider) {
+            this.df = df;
+            this.parentEvent = parentEvent;
+            this.nameProvider = nameProvider;
+        }
+
+        @Override
+        public void visit(OWLObjectSomeValuesFrom ce) {
+            if (ce.getProperty().equals(includedProperty)) {
+                if (subEvent == null) {
+                    subEvent = df.getOWLNamedIndividual(nameProvider.getName(parentEvent));
+                    axioms.add(df.getOWLObjectPropertyAssertionAxiom(duringProperty, subEvent, parentEvent));
+                    ce.getFiller().accept(this);
+                }
+                else {
+                    // nesting - see Attack_on_Rishi_Station
+                    OWLClassToIndividual nest = new OWLClassToIndividual(df, subEvent, nameProvider);
+                    ce.accept(nest);
+                    axioms.addAll(nest.getAxioms());
+                }
+            }
+            else {
+                if (subEvent != null) {
+                    axioms.add(df.getOWLClassAssertionAxiom(ce, subEvent));
+                }
+                else {
+                    System.err.println(render(parentEvent) + " Unexpected some: " + render(ce));
+                }
+            }
+        }
+
+        @Override
+        public void visit(OWLObjectIntersectionOf ce) {
+            ce.operands().forEach(c -> c.accept(this));
+        }
+
+        @Override
+        public void visit(OWLClass ce) {
+            axioms.add(df.getOWLClassAssertionAxiom(ce, subEvent));
+        }
+
+        @Override
+        public void visit(OWLObjectHasValue ce) {
+            axioms.add(df.getOWLObjectPropertyAssertionAxiom(ce.getProperty(), subEvent, ce.getFiller()));
+        }
+
+        @Override
+        public void visit(OWLObjectUnionOf ce) {
+            throw new RuntimeException(render(parentEvent) + " - Unhandled Union: " + render(ce));
+        }
+
+        @Override
+        public void visit(OWLObjectComplementOf ce) {
+            if (subEvent != null) {
+                axioms.add(df.getOWLClassAssertionAxiom(ce, subEvent));
+            }
+            else {
+                System.err.println(render(parentEvent) + " Unexpected complement: " + render(ce));
+            }
+        }
+
+        @Override
+        public void visit(OWLObjectAllValuesFrom ce) {
+            throw new RuntimeException(render(parentEvent) + " - Unhandled Only: " + render(ce));
+        }
+
+        @Override
+        public void visit(OWLObjectMinCardinality ce) {
+            throw new RuntimeException(render(parentEvent) + " - Unhandled restriction: " + render(ce));
+        }
+
+        @Override
+        public void visit(OWLObjectExactCardinality ce) {
+            axioms.add(df.getOWLClassAssertionAxiom(ce, subEvent));
+        }
+
+        @Override
+        public void visit(OWLObjectMaxCardinality ce) {
+            throw new RuntimeException(render(parentEvent) + " - Unhandled restriction: " + render(ce));
+        }
+
+        @Override
+        public void visit(OWLObjectHasSelf ce) {
+            throw new RuntimeException(render(parentEvent) + " - Unhandled restriction: " + render(ce));
+        }
+
+        @Override
+        public void visit(OWLObjectOneOf ce) {
+            throw new RuntimeException(render(parentEvent) + " - Unhandled restriction: " + render(ce));
+        }
+
+        @Override
+        public void visit(OWLDataSomeValuesFrom ce) {
+            throw new RuntimeException(render(parentEvent) + " - Unhandled restriction: " + render(ce));
+        }
+
+        @Override
+        public void visit(OWLDataAllValuesFrom ce) {
+            throw new RuntimeException(render(parentEvent) + " - Unhandled restriction: " + render(ce));
+        }
+
+        @Override
+        public void visit(OWLDataHasValue ce) {
+            throw new RuntimeException(render(parentEvent) + " - Unhandled restriction: " + render(ce));
+        }
+
+        @Override
+        public void visit(OWLDataMinCardinality ce) {
+            throw new RuntimeException(render(parentEvent) + " - Unhandled restriction: " + render(ce));
+        }
+
+        @Override
+        public void visit(OWLDataExactCardinality ce) {
+            throw new RuntimeException(render(parentEvent) + " - Unhandled restriction: " + render(ce));
+        }
+
+        @Override
+        public void visit(OWLDataMaxCardinality ce) {
+            throw new RuntimeException(render(parentEvent) + " - Unhandled restriction: " + render(ce));
+        }
+
+        public Set<OWLAxiom> getAxioms() {
+            return axioms;
+        }
+    };
+}
---
+++ src/test/java/com/nickd/sw/TestAboxRefactor.java
@@ -0,0 +1,127 @@
+package com.nickd.sw;
+
+import com.nickd.sw.util.TestHelper;
+import com.nickd.sw.util.TestOntologiesIRIMapper;
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+import org.semanticweb.owlapi.model.*;
+
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.hasItem;
+import static org.junit.Assert.assertThat;
+
+public class TestAboxRefactor extends TestCase {
+
+    private static TestHelper helper;
+    private static List<OWLOntologyChange> changes;
+
+    private static OWLObjectProperty includedProperty;
+    private static OWLObjectProperty duringProperty;
+    private static OWLDataFactory df;
+
+    private static AboxRefactor refactor;
+
+    // One time load
+    public static Test suite() throws OWLOntologyCreationException {
+        helper = new TestHelper(
+                new TestSuite(TestAboxRefactor.class),
+                Helper.BASE + "/test-refactor-abox.owl.ttl",
+                new TestOntologiesIRIMapper());
+        df = helper.df();
+
+        includedProperty = helper.prop("included");
+        duringProperty = helper.prop("during");
+
+        refactor = new AboxRefactor(includedProperty, duringProperty);
+
+        changes = refactor.run(helper.onts(), helper.df());
+
+        return helper;
+    }
+
+    public void testRefactorRemovesSubclassAssertions() {
+        RemoveAxiom expected = new RemoveAxiom(helper.ont(), df.getOWLClassAssertionAxiom(
+                helper.mos("included some(Killing and (of value Droidbait))"),
+                helper.ind("Attack_on_Rishi_Station_Simple")));
+
+        assertThat(changes, hasItem(expected));
+    }
+
+    public void testRefactorCreatesNewSubEvent() {
+        AddAxiom expected = new AddAxiom(helper.ont(), df.getOWLClassAssertionAxiom(
+                helper.cls("Killing"),
+                helper.ind("_attack_on_rishi_station_simple0")
+        ));
+
+        assertThat(changes, hasItem(expected));
+    }
+
+    public void testRefactorLinksSubEventBackToEvent() {
+        AddAxiom expected = new AddAxiom(helper.ont(), df.getOWLObjectPropertyAssertionAxiom(
+                duringProperty,
+                helper.ind("_attack_on_rishi_station_simple0"),
+                helper.ind("Attack_on_Rishi_Station_Simple")
+        ));
+
+        assertThat(changes, hasItem(expected));
+    }
+
+    public void testRefactorGeneratesPropertyAssertionsFromHasValue() {
+        AddAxiom expected = new AddAxiom(helper.ont(), df.getOWLObjectPropertyAssertionAxiom(
+                helper.prop("of"),
+                helper.ind("_attack_on_rishi_station_simple0"),
+                helper.ind("Droidbait")
+        ));
+
+        assertThat(changes, hasItem(expected));
+    }
+
+    public void testRefactorGeneratesClassAssertionFromSome() {
+        AddAxiom expected = new AddAxiom(helper.ont(), df.getOWLClassAssertionAxiom(
+                helper.mos("of some BX-series_droid_commando"),
+                helper.ind("_attack_on_rishi_station_some0")
+        ));
+
+        assertThat(changes, hasItem(expected));
+    }
+
+    public void testRefactorMultipleSubEvents() {
+        AddAxiom expected1 = new AddAxiom(helper.ont(), df.getOWLObjectPropertyAssertionAxiom(
+                duringProperty,
+                helper.ind("_attack_on_rishi_station_both0"),
+                helper.ind("Attack_on_Rishi_Station_Both")
+        ));
+        AddAxiom expected2 = new AddAxiom(helper.ont(), df.getOWLObjectPropertyAssertionAxiom(
+                duringProperty,
+                helper.ind("_attack_on_rishi_station_both1"),
+                helper.ind("Attack_on_Rishi_Station_Both")
+        ));
+
+
+        assertThat(changes, hasItem(expected1));
+        assertThat(changes, hasItem(expected2));
+    }
+
+    public void testRefactorGeneratesNestedEvents() {
+        String subEventName = "_attack_on_rishi_station_nested0";
+        String subSubEventName = "__attack_on_rishi_station_nested00";
+
+        AddAxiom during = new AddAxiom(helper.ont(), df.getOWLObjectPropertyAssertionAxiom(
+                duringProperty,
+                helper.ind(subSubEventName),
+                helper.ind(subEventName)
+        ));
+
+        AddAxiom expected = new AddAxiom(helper.ont(), df.getOWLClassAssertionAxiom(
+                helper.mos("of some (BX-series_droid_commando and (disguisedAs some Trooper))"),
+                helper.ind(subSubEventName)
+        ));
+
+        assertThat(changes, hasItem(during));
+        assertThat(changes, hasItem(expected));
+    }
+}
+
+

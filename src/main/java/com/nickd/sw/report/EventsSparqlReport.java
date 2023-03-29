b6848b9216cbb03e94fdbd0f214f56b2ca517b3e
nickdrummond <nickdrummond@yahoo.com>
2023-03-30 00:07:57 +0100

    Event Sparql tweaks

--- src/main/java/com/nickd/sw/report/EventsSparqlReport.java
+++ src/main/java/com/nickd/sw/report/EventsSparqlReport.java
@@ -3,6 +3,7 @@ package com.nickd.sw.report;
 import org.apache.jena.query.*;
 
 import java.io.IOException;
+import java.util.Map;
 
 public class EventsSparqlReport {
 
@@ -10,10 +11,33 @@ public class EventsSparqlReport {
 
         final SparqlReport report = new SparqlReport("ontologies/events.owl.ttl");
 
+        eventsByLocation(report, "sw:Outer_Rim");
+
+        events(report);
+    }
+
+    private static void events(final SparqlReport report) throws IOException {
         final Query query = report.loadQuery("sparql/events.sparql");
 
         try (QueryExecution qexec = report.run(query)) {
-            ResultSetFormatter.out(qexec.execSelect());
+            ResultSet qresults = qexec.execSelect();
+            ResultSetFormatter.out(qresults);
+            System.out.println(qresults.getRowNumber() + " results");
+        }
+    }
+
+    private static void eventsByLocation(final SparqlReport report, final String generalLocation) throws IOException {
+
+        final Query query = report.loadQuery("sparql/eventsByLocation.sparql");
+
+        Map<String, String> bindings = Map.of("generalLocation", generalLocation);
+
+        try (QueryExecution qexec = report.runWithBindings(query, bindings)) {
+            ResultSet qresults = qexec.execSelect();
+
+            System.out.println("Location: " + generalLocation);
+            ResultSetFormatter.out(qresults);
+            System.out.println(qresults.getRowNumber() + " results");
         }
     }
 }
--- src/main/java/com/nickd/sw/report/SparqlReport.java
+++ src/main/java/com/nickd/sw/report/SparqlReport.java
@@ -10,6 +10,7 @@ import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 public class SparqlReport {
@@ -35,5 +36,17 @@ public class SparqlReport {
 
     public QueryExecution run(Query query) {
         return QueryExecutionFactory.create(query, rdfsInfModel);
+    }
+
+    public QueryExecution runWithBindings(Query query, Map<String, String> bindings) {
+        QuerySolutionMap initialBinding = new QuerySolutionMap();
+        bindings.forEach((key, value) -> initialBinding.add(key, getResource(query, value)));
+        return QueryExecutionFactory.create(query, rdfsInfModel, initialBinding);
+    }
+
+    private Resource getResource(Query query, String prefixedName) {
+        String[] pre = prefixedName.split(":");
+        String uri = query.getPrefix(pre[0]);
+        return rdfsInfModel.getResource(uri + pre[1]);
     }
 }
